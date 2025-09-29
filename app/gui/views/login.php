354f2becf5922e06eac82fa44c70699beb5c7bf8
Giuseppe Di Guglielmo <giuseppe.diguglielmo@guavestudios.com>
2025-09-29 11:27:54 +0200

    Remove jQuery script inclusion

--- app/gui/views/login.php
+++ app/gui/views/login.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Guave Lingua - Login</title>
+		<title>Languave - Login</title>
 		<base href="//<?= $_SERVER['HTTP_HOST'].config::get('base') ?>">
 		<link href="gui/css/styles.css" rel="stylesheet" type="text/css">
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 	</head>
 	<body>
 		<div id="wrapper" class="centered">
